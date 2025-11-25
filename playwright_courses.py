5dc08f369408f23603dd81892bc56785ce301788
egormir2188 <yaegormir2188@gmail.com>
2025-11-25 08:05:19 +0400

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

--- playwright_courses.py
+++ playwright_courses.py
@@ -11,19 +11,13 @@ with sync_playwright() as playwright:
     )
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.focus()
-    for char in 'user.name@gmail.com':
-        page.keyboard.type(char, delay=50)
+    email_input.fill('user.name@gmail.com')
 
     username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.focus()
-    for char in 'username':
-        page.keyboard.type(char, delay=50)
+    username_input.fill('username')
 
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.focus()
-    for char in 'password':
-        page.keyboard.type(char, delay=50)
+    password_input.fill('password')
 
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
@@ -33,9 +27,23 @@ with sync_playwright() as playwright:
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='./browser-state.json')
-
     page = context.new_page()
 
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+    course_header = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(course_header).to_be_visible()
+    expect(course_header).to_have_text('Courses')
+
+    course_list_title = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(course_list_title).to_be_visible()
+    expect(course_list_title).to_have_text('There is no results')
+
+    empty_block_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_block_icon).to_be_visible()
+
+    course_empty_list_description = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(course_empty_list_description).to_be_visible()
+    expect(course_empty_list_description).to_have_text('Results from the load test pipeline will be displayed here')
 
     page.wait_for_timeout(3000)
