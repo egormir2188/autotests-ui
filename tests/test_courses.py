2683db44ca8f4b81c01b6284513dad1c46c4f332
egormir2188 <yaegormir2188@gmail.com>
2025-11-26 12:48:07 +0400

    Маркировки Pytest. Практика работы с pytest маркировками

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -8,6 +8,29 @@ from playwright.sync_api import sync_playwright, expect
 def test_empty_courses_list():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto(
+            'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration'
+        )
+
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill('user.name@gmail.com')
+
+        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        username_input.fill('username')
+
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill('password')
+
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.click()
+
+        context.storage_state(path='browser-state.json')
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
         context = browser.new_context(storage_state='./browser-state.json')
         page = context.new_page()
 
@@ -27,3 +50,5 @@ def test_empty_courses_list():
         course_empty_list_description = page.get_by_test_id('courses-list-empty-view-description-text')
         expect(course_empty_list_description).to_be_visible()
         expect(course_empty_list_description).to_have_text('Results from the load test pipeline will be displayed here')
+
+        # page.wait_for_timeout(3000)
