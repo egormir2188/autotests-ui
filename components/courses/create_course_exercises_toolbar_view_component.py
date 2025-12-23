2dc5f2d3ab38b7bb6cf2bfd7eed613b443abef20
egormir2188 <yaegormir2188@gmail.com>
2025-12-23 14:59:12 +0400

    Практическое применение PageFactory в автотестах

--- components/courses/create_course_exercises_toolbar_view_component.py
+++ components/courses/create_course_exercises_toolbar_view_component.py
@@ -1,5 +1,7 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
+from elements.text import Text
+from elements.button import Button
 from components.base_component import BaseComponent
 
 
@@ -7,15 +9,17 @@ class CreateCourseExercisesToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.title = page.get_by_test_id('create-course-exercises-box-toolbar-title-text')
-        self.create_exercise_button = page.get_by_test_id('create-course-exercises-box-toolbar-create-exercise-button')
+        self.title = Text(page, 'create-course-exercises-box-toolbar-title-text', 'Title')
+        self.create_exercise_button = Button(
+            page, 'create-course-exercises-box-toolbar-create-exercise-button', 'Create exercise'
+        )
 
     def check_visible(self):
-        expect(self.title).to_be_visible()
-        expect(self.title).to_have_text('Exercises')
+        self.title.check_visible()
+        self.title.to_have_text('Exercises')
 
-        expect(self.create_exercise_button).to_be_visible()
-        expect(self.create_exercise_button).to_be_enabled()
+        self.create_exercise_button.check_visible()
+        self.create_exercise_button.to_be_enabled()
 
     def click_create_exercise_button(self):
         self.create_exercise_button.click()
--- components/courses/create_course_form_component.py
+++ components/courses/create_course_form_component.py
@@ -1,5 +1,7 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
+from elements.input import Input
+from elements.textarea import Textarea
 from components.base_component import BaseComponent
 
 
@@ -7,15 +9,15 @@ class CreateCourseFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.title_input = page.get_by_test_id('create-course-form-title-input').locator('input')
-        self.estimated_time_input = (
-            page.get_by_test_id('create-course-form-estimated-time-input').locator('input')
+        self.title_input = Input(page, 'create-course-form-title-input', 'Course title')
+        self.estimated_time_input = Input(
+            page, 'create-course-form-estimated-time-input', 'Course estimated time'
         )
-        self.description_textarea = (
-            page.get_by_test_id('create-course-form-description-input').locator('textarea').first
+        self.description_textarea = Textarea(
+            page, 'create-course-form-description-input', 'Course description'
         )
-        self.max_score_input = page.get_by_test_id('create-course-form-max-score-input').locator('input')
-        self.min_score_input = page.get_by_test_id('create-course-form-min-score-input').locator('input')
+        self.max_score_input = Input(page, 'create-course-form-max-score-input', 'Max score')
+        self.min_score_input = Input(page, 'create-course-form-min-score-input', 'Min score')
         
     def fill(
             self,
@@ -39,17 +41,17 @@ class CreateCourseFormComponent(BaseComponent):
             max_score: str,
             min_score: str
     ):
-        expect(self.title_input).to_be_visible()
-        expect(self.title_input).to_have_value(title)
+        self.title_input.check_visible()
+        self.title_input.to_have_value(title)
 
-        expect(self.estimated_time_input).to_be_visible()
-        expect(self.estimated_time_input).to_have_value(estimated_time)
+        self.estimated_time_input.check_visible()
+        self.estimated_time_input.to_have_value(estimated_time)
 
-        expect(self.description_textarea).to_be_visible()
-        expect(self.description_textarea).to_have_value(description)
+        self.description_textarea.check_visible()
+        self.description_textarea.to_have_value(description)
 
-        expect(self.max_score_input).to_be_visible()
-        expect(self.max_score_input).to_have_value(max_score)
+        self.max_score_input.check_visible()
+        self.max_score_input.to_have_value(max_score)
 
-        expect(self.min_score_input).to_be_visible()
-        expect(self.min_score_input).to_have_value(min_score)
+        self.min_score_input.check_visible()
+        self.min_score_input.to_have_value(min_score)
