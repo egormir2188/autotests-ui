8eee662c031df5cf20f39e9502635e75979d49bd
egormir2188 <yaegormir2188@gmail.com>
2026-08-08 02:44:40 +0400

    Добавление видео в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,25 +1,17 @@
-import allure
 import pytest
 
 from typing import Any, Generator
 from _pytest.fixtures import SubRequest
 from playwright.sync_api import Page, Playwright
 
+from tools.playwright.pages import initialize_playwright_page
 from pages.authentication.registration_page import RegistrationPage
 
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Generator[Page, Any, None]:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    yield from initialize_playwright_page(playwright=playwright, test_name=request.node.name)
 
-    yield browser.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(source=f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
@@ -41,19 +33,13 @@ def initialize_browser_state(playwright: Playwright):
     context.close()
     browser.close()
 
+
 @pytest.fixture
 def chromium_page_with_state(
         request: SubRequest, playwright: Playwright, initialize_browser_state
 ) -> Generator[Page, Any, None]:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='./browser-state.json')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-    yield context.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-
-    context.close()
-    browser.close()
-
-    allure.attach.file(source=f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    yield from initialize_playwright_page(
+        playwright=playwright,
+        test_name=request.node.name,
+        storage_state='./browser-state.json'
+    )
